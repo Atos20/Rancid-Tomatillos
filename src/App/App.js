15ea41ce36c23f96c1d4b069312ba186f4e2b223
Orlando Murcio <ato.murcio@gmail.com>
2020-10-06 17:07:09 -0600

    Define Log-in form

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {LoginForm} from '../LoginForm/LoginForm'
-import './App.css';
+import './App.scss';
 
 class App extends Component{
   constructor() {
@@ -11,7 +11,7 @@ class App extends Component{
     return (
       <section className="App">
         <h1>Rancid Tomatillos</h1>
-        <LoginForm />
+        {/* <LoginForm /> */}
       </section>
     );
   }
