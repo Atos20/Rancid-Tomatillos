ab483df565489be570ae64600017b78472726d26
Ian Holladay <holladayian@gmail.com>
2020-10-06 15:52:21 -0800

    Move homepage data into respective HomePage file

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,23 @@
 import React, {Component} from 'react';
 import {LoginForm} from '../LoginForm/LoginForm'
-import './App.scss';
+import { Homepage } from '../HomePage/HomePage'
+import './App.css';
+// import './App.scss';
 
-let styles = {
-  poster: {
-    height: 185,
-    width: 145
-  },
-  title: {
-    width: 100,
-    height: 65,
-  },
-  rating: {
-    width: 100,
-    height: 45,
-  }
-}
+// let styles = {
+//   poster: {
+//     height: 185,
+//     width: 145
+//   },
+//   title: {
+//     width: 100,
+//     height: 65,
+//   },
+//   rating: {
+//     width: 100,
+//     height: 45,
+//   }
+// }
 
 class App extends Component{
   constructor() {
@@ -25,10 +27,13 @@ class App extends Component{
   render(){
     return (
       <section className="App">
-        <h1>Rancid Tomatillos</h1>
-        {/* <LoginForm /> */}
+      <Homepage />
+      {/* <LoginForm /> */}
+        {/* <h1>Rancid Tomatillos</h1>
+        <button>Login</button>
+        <LoginForm />
         <section className="Card-area">
-        {/* fetch GET movies
+        fetch GET movies
         .map(
         <section className="Card" id="index">
           <section name="title" style={styles.title}>Title</section>
@@ -36,13 +41,13 @@ class App extends Component{
           <img src="" style={styles.poster}></img>
         </section>
         )
-         */}
+        
         <section className="Card">
           <div style={styles.title}>Title</div>
           <div style={styles.rating}>Rating</div>
           <img src="https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg" style={styles.poster}></img>
         </section>
-      </section>
+      </section> */}
     </section>
     );
   }
