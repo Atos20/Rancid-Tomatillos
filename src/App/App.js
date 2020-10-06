ab4501b30040bce2a93788b0824f8cd6c5f60083
Orlando Murcio <ato.murcio@gmail.com>
2020-10-06 15:35:16 -0600

    Change name of header

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Header</h1>
+      {/* lets change the name of this to what we want the header of our webpage to be, perhaps Rancid Tomatillos */}
     </div>
   );
 }
