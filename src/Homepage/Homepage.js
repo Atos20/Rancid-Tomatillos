de0881eb09eb132f73f6a547180222286cd87b97
Orlando Murcio <ato.murcio@gmail.com>
2020-10-06 19:54:13 -0600

    Create nsvbar

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -9,20 +9,29 @@ export class Homepage extends Component {
     render(){
 
         return(
-        <> 
         <nav className="nav-bar">
-          <h1 className="main-title">Rancid Tomatillos</h1>
-          <button className="Login">Login</button>
+          <h1 className="main-title"><i className="fas fa-seedling"></i>Rancid Tomatillos</h1>
+
+          <div className="search-container">
+            <i className="fas fa-search"></i>
+            <label htmlFor="search-bar">
+            <input
+              className="search-bar"
+              type="text" 
+              className="search-bar" 
+              id="searh-bar"/>
+            </label>
+          </div>
+          <button className="Login-button">Login</button>
         </nav>
 
-          <section className="Card-area"></section>
-           
+         /* <section className="Card-area">
           <article className="Card">
-          <div >Title</div>
-          <div >Rating</div>
-          <img src="https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg" ></img>
+            <div >Title</div>
+            <div >Rating</div>
+            <img src="https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg" ></img>
           </article> 
-      </>
+        </section> */
     )
-    }
+  }
 }
