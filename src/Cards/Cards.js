e8a9240a2184229f5a7529ef3a44da7f097f6550
Orlando Murcio <ato.murcio@gmail.com>
2020-10-07 13:57:03 -0600

    Initial Commit

---
+++ src/Cards/Cards.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Cards.scss'
+
+export default class Cards extends Component {
+  render() {
+    return (
+     <div>
+            
+    </div>
+  )
+}
+}
