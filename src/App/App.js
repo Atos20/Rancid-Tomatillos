4721341a8a49cfe583a5506d672a5afc0f0709ae
Orlando Murcio <ato.murcio@gmail.com>
2020-10-06 23:32:02 -0600

    Add Movie section

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {LoginForm} from '../LoginForm/LoginForm'
 import { Homepage } from '../Homepage/Homepage'
+import { MovieSection } from '../MovieSection/MovieSection'
 import './App.scss';
 
 class App extends Component{
@@ -10,10 +11,11 @@ class App extends Component{
 
   render(){
     return (
-     
+     <>
       <Homepage />
-      /* <LoginForm /> */
-    
+      <MovieSection />
+      {/* <LoginForm />  */}
+    </>
     );
   }
 }
---
+++ src/MovieSection/MovieSection.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './MovieSection.scss';
+
+export function MovieSection() {
+
+  return(
+    <div className="movie-section">
+      <h1 className="movie-day">Rate your next movie</h1>
+    </div>
+  )
+}
