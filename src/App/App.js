51b07c8f3eeb4b021cf4800960db70c787d3bd15
Orlando Murcio <ato.murcio@gmail.com>
2020-10-06 18:39:27 -0600

    Change name

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,7 @@
 import React, {Component} from 'react';
 import {LoginForm} from '../LoginForm/LoginForm'
 import { Homepage } from '../Homepage/Homepage'
-import './App.css';
-// import './App.scss';
-
-// let styles = {
-//   poster: {
-//     height: 185,
-//     width: 145
-//   },
-//   title: {
-//     width: 100,
-//     height: 65,
-//   },
-//   rating: {
-//     width: 100,
-//     height: 45,
-//   }
-// }
+import './App.scss';
 
 class App extends Component{
   constructor() {
@@ -29,25 +13,6 @@ class App extends Component{
       <section className="App">
       <Homepage />
       {/* <LoginForm /> */}
-        {/* <h1>Rancid Tomatillos</h1>
-        <button>Login</button>
-        <LoginForm />
-        <section className="Card-area">
-        fetch GET movies
-        .map(
-        <section className="Card" id="index">
-          <section name="title" style={styles.title}>Title</section>
-          <section name="rating" style={styles.rating}>Rating</section>
-          <img src="" style={styles.poster}></img>
-        </section>
-        )
-        
-        <section className="Card">
-          <div style={styles.title}>Title</div>
-          <div style={styles.rating}>Rating</div>
-          <img src="https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg" style={styles.poster}></img>
-        </section>
-      </section> */}
     </section>
     );
   }
