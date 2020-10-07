594127cc8c7f0baaba58db54380f67c07f69fdea
Orlando Murcio <ato.murcio@gmail.com>
2020-10-06 23:52:09 -0600

    Add welcoming message

--- src/MovieSection/MovieSection.js
+++ src/MovieSection/MovieSection.js
@@ -5,7 +5,9 @@ export function MovieSection() {
 
   return(
     <div className="movie-section">
-      <h1 className="movie-day">Rate your next movie</h1>
+      <h1 className="user-welcome">Welcome!</h1>
+      <h2 className="user-name">Ian Holliday</h2>
+      <h3 className="movie-day">Rate your next movie</h3>
     </div>
   )
 }
