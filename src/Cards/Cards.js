080557829fcc328cb2165fb853c45a99f93ec942
Orlando Murcio <ato.murcio@gmail.com>
2020-10-07 15:03:34 -0600

    Style cards

--- src/Cards/Cards.js
+++ src/Cards/Cards.js
@@ -2,11 +2,107 @@ import React, { Component } from 'react'
 import './Cards.scss'
 
 export default class Cards extends Component {
-  render() {
-    return (
-     <div>
-            
-    </div>
-  )
+
+    render() {
+      return (
+        <>
+          <h1 className="cards-title">All movies</h1>
+          <section className="card-area">
+
+            <div className="card">
+              <h1 className="movie-title">Cats & Dogs 3: Paws Unite</h1>
+              <h2 className="movie-release">(2003)</h2>
+              <div className="rating-container">
+                <h2 className="movie-rating">Rating</h2>
+                <h2 className="movie-rating">9</h2>
+              </div>
+              <img className="movie-img"src="https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg" ></img>
+            </div> 
+
+            <div className="card">
+              <h1 className="movie-title">Cats & Dogs 3: Paws Unite</h1>
+              <h2 className="movie-release">(2002)</h2>
+              <div className="rating-container">
+                <h2 className="movie-rating">Rating</h2>
+                <h2 className="movie-rating">10</h2>
+              </div>
+              <img className="movie-img"src="https://image.tmdb.org/t/p/original//gxK2lB1w8an5ViPXzisDsRsyHr0.jpg" ></img>
+            </div> 
+
+            <div className="card">
+              <h1 className="movie-title">Cats & Dogs 3: Paws Unite</h1>
+              <h2 className="movie-release">(2019)</h2>
+              <div className="rating-container">
+                <h2 className="movie-rating">Rating</h2>
+                <h2 className="movie-rating">10</h2>
+              </div>
+              <img className="movie-img"src="https://image.tmdb.org/t/p/original//7D430eqZj8y3oVkLFfsWXGRcpEG.jpg" ></img>
+            </div> 
+
+
+            <div className="card">
+              <h1 className="movie-title">Cats & Dogs 3: Paws Unite</h1>
+              <h2 className="movie-release">(2003)</h2>
+              <div className="rating-container">
+                <h2 className="movie-rating">Rating</h2>
+                <h2 className="movie-rating">9</h2>
+              </div>
+              <img className="movie-img"src="https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg" ></img>
+            </div> 
+
+            <div className="card">
+              <h1 className="movie-title">Cats & Dogs 3: Paws Unite</h1>
+              <h2 className="movie-release">(2002)</h2>
+              <div className="rating-container">
+                <h2 className="movie-rating">Rating</h2>
+                <h2 className="movie-rating">10</h2>
+              </div>
+              <img className="movie-img"src="https://image.tmdb.org/t/p/original//gxK2lB1w8an5ViPXzisDsRsyHr0.jpg" ></img>
+            </div> 
+
+            <div className="card">
+              <h1 className="movie-title">Cats & Dogs 3: Paws Unite</h1>
+              <h2 className="movie-release">(2019)</h2>
+              <div className="rating-container">
+                <h2 className="movie-rating">Rating</h2>
+                <h2 className="movie-rating">10</h2>
+              </div>
+              <img className="movie-img"src="https://image.tmdb.org/t/p/original//7D430eqZj8y3oVkLFfsWXGRcpEG.jpg" ></img>
+            </div> 
+
+            <div className="card">
+              <h1 className="movie-title">Cats & Dogs 3: Paws Unite</h1>
+              <h2 className="movie-release">(2003)</h2>
+              <div className="rating-container">
+                <h2 className="movie-rating">Rating</h2>
+                <h2 className="movie-rating">9</h2>
+              </div>
+              <img className="movie-img"src="https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg" ></img>
+            </div> 
+
+            <div className="card">
+              <h1 className="movie-title">Cats & Dogs 3: Paws Unite</h1>
+              <h2 className="movie-release">(2002)</h2>
+              <div className="rating-container">
+                <h2 className="movie-rating">Rating</h2>
+                <h2 className="movie-rating">10</h2>
+              </div>
+              <img className="movie-img"src="https://image.tmdb.org/t/p/original//gxK2lB1w8an5ViPXzisDsRsyHr0.jpg" ></img>
+            </div> 
+
+            <div className="card">
+              <h1 className="movie-title">Cats & Dogs 3: Paws Unite</h1>
+              <h2 className="movie-release">(2019)</h2>
+              <div className="rating-container">
+                <h2 className="movie-rating">Rating</h2>
+                <h2 className="movie-rating">10</h2>
+              </div>
+              <img className="movie-img"src="https://image.tmdb.org/t/p/original//7D430eqZj8y3oVkLFfsWXGRcpEG.jpg" ></img>
+            </div> 
+
+          </section> 
+        </>
+    )
+  }
 }
-}
+
