a5bc52411c5aa5d6223e9241d017751d1f844d75
Orlando Murcio <ato.murcio@gmail.com>
2020-10-07 18:34:14 -0600

    Disaply login status

--- src/App/App.js
+++ src/App/App.js
@@ -6,11 +6,17 @@ import './App.scss';
 class App extends Component{
   constructor() {
     super()
+
+    this.state = {
+      isLoggedIn : false
+    }
   }
 
   render(){
+    const { isLoggedIn } = this.state
     return (
      <>
+     <h1 className="login-info">The user is <b>{isLoggedIn ? 'currently' : 'not'}</b> logged in.</h1>
       <Homepage />
       {/* <LoginForm />  */}
     </>
