8ca4b4273ff990e24ed169511a3dfab7f79d2c16
Ian Holladay <holladayian@gmail.com>
2020-10-08 13:58:27 -0800

    unfinished: Cards test

--- src/Cards/Cards.test.js
+++ src/Cards/Cards.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, section } from '@testing-library/react';
 import Cards from './Cards.js';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
@@ -8,5 +8,6 @@ describe('Cards', () => {
   it('should render cards', () => {
     render(<Cards />)
     expect(screen.getByText('All movies')).toBeInTheDocument();
+    expect(screen.getByRole(section, {className: 'card-area'})).toBeInTheDocument();
   })
 })
