a4dd2421510d6223f58617261b7d9719f15db7eb
Ian Holladay <holladayian@gmail.com>
2020-10-08 16:28:47 -0800

    Create Card test

--- src/Cards/Cards.test.js
+++ src/Cards/Cards.test.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { render, screen, section } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Cards from './Cards.js';
 import '@testing-library/jest-dom';
-import userEvent from '@testing-library/user-event';
 
 describe('Cards', () => {
   it('should render cards', () => {
     render(<Cards />)
     expect(screen.getByText('All movies')).toBeInTheDocument();
-    expect(screen.getByRole(section, {className: 'card-area'})).toBeInTheDocument();
   })
 })
