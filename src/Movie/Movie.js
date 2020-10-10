5cbe5c874ba4a09cddeb5c00c76c5ae1f8b598f0
Orlando Murcio <ato.murcio@gmail.com>
2020-10-09 20:50:58 -0600

    Get movie Id from movie card

---
+++ src/Movie/Movie.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Movie.scss';
+
+export default function Movie() {
+    return (
+        <div>
+            <h1 className="name">Hola</h1>
+        </div>
+    )
+}
