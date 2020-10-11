c4235d677f1c272c9c19a47ed24c1dd99767d799
Ian Holladay <holladayian@gmail.com>
2020-10-10 19:46:09 -0800

    Remove props from ErrorBoundary

--- src/ErrorMessage/ErrorMessage.js
+++ src/ErrorMessage/ErrorMessage.js
@@ -2,10 +2,6 @@ import React, {Component} from 'react';
 import './ErrorMessage.scss';
 
 export class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props);
-    console.log(props)
-  }
 
   render() {
     return (
