2c69137b30d3f14eb01331a51077bcb562d49562
Ian Holladay <holladayian@gmail.com>
2020-10-11 18:27:06 -0800

    remove comments

--- src/SortMovies/SortMovies.test.js
+++ src/SortMovies/SortMovies.test.js
@@ -5,22 +5,8 @@ import '@testing-library/jest-dom';
 
 describe('SortMovies', () => {
   it('should render the sorted movies', () => {
-    // const fakeSortedMovies = {[
-    //     {average_rating: 9,
-    //     backdrop_path: "fake/backdrop/path",
-    //     id: 694919,
-    //     poster_path: "fake/poster/path",
-    //     release_date: "2020-09-29",
-    //     title: "Money Plane"},
-    //     {average_rating: 2,
-    //     backdrop_path: "fake/backdrop/path",
-    //     id: 444444,
-    //     poster_path: "fake/poster/path",
-    //     release_date: "2019-08-20",
-    //     title: "Currency Aircraft"}
-    //   ]};
+
     render(<SortMovies />);
-    // render(<SortMovies sortMovies={fakeSortedMovies}/>);
     expect(screen.getByText("--none--")).toBeInTheDocument();
     expect(screen.getByText("Rating descending")).toBeInTheDocument();
     expect(screen.getByText("Rating ascending")).toBeInTheDocument();
