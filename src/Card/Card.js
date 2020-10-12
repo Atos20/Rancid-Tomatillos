82e27856cf5cdd2a09a44aa735b975426f0e8c50
Ian Holladay <holladayian@gmail.com>
2020-10-12 15:01:58 -0800

    Wrap entire movie card in link

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -12,21 +12,21 @@ export class Card extends Component{
     const {id, title, release_date, average_rating, poster_path} = this.props.movies
     return (
       <>
-        <div key={id} id={id} className="card">
-          <h1 className="movie-title">{title}</h1>
-          <h2 className="movie-release">{release_date}</h2>
-          <div className="rating-container">
-            <h2 className="movie-rating">Rating</h2>
-            <h2 className="movie-rating">{average_rating}</h2>
-          </div>
-          <Link className="movie-img" to={`/movies/${id}`}>
-            <img 
-              id={id} 
-              className="movie-img" src={poster_path} 
-              onClick={this.displayMovie}>
-            </img>
-          </Link>
-        </div> 
+        <Link className="movie-img" to={`/movies/${id}`}>
+          <div key={id} id={id} className="card">
+            <h1 className="movie-title">{title}</h1>
+            <h2 className="movie-release">{release_date}</h2>
+            <div className="rating-container">
+              <h2 className="movie-rating">Rating</h2>
+              <h2 className="movie-rating">{average_rating}</h2>
+            </div>
+              <img 
+                id={id} 
+                className="movie-img" src={poster_path} 
+                onClick={this.displayMovie}>
+              </img>
+          </div> 
+        </Link>
       </>
     )
   }
