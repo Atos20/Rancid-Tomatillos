0c5911a993adb7ce91514dbe3699d8188653f3bf
Orlando Murcio <ato.murcio@gmail.com>
2020-10-13 16:36:38 -0600

    Style movie card

--- src/MoviePage/MovieCard/MovieCard.js
+++ src/MoviePage/MovieCard/MovieCard.js
@@ -3,38 +3,53 @@ import './MovieCard.scss'
 import moment from 'moment'
 
 export default function MovieCard({movieDetails}) {
-  const genreList = movieDetails.genres.map((genre, i) => <li key={i}>{genre}</li>)
+  const genreList = movieDetails.genres.map((genre, i) => <p className='single-genre'key={i}> {genre} </p>)
   return (
-    <div  className="d movie-card-container">
-      <h1 className="genre-title">Rating</h1>
-      <h1 className="average-rating">{movieDetails.average_rating}</h1>
-      
-      <div className="runtime-container">
-        <h1 className="runtime-title">Time</h1>
-        <p className="runtime">{movieDetails.runtime === 0 ? 'No runtime found' : `${movieDetails.runtime} mins.`}</p>
-      </div>
+    <div  className="movie-card-container">
 
-      <div className="budget-container">
-        <h1 className="budget-title">budget</h1>
-        <p className="budget">{movieDetails.budget === 0 ? 'No budget found' : `$ ${movieDetails.budget}`}</p>
-      </div>
-
-      <div className="revenue-container">
-        <h1 className="revenue-title">budget</h1>
-        <p className="revenue">{movieDetails.revenue === 0 ? 'No revenue found' : `$ ${movieDetails.revenue}`}</p>
-      </div>
+      <div className="inner-container">
 
       <div className="release-container">
-        <h1 className="release-title">release date</h1>
-        <p className="release">{movieDetails.release_date === 0 ? 'No release date found' : moment(movieDetails.release_date).format("MMM Do YY")}</p>
-      </div>
+          <h1 className="overview-container">Overview</h1>
+          <p className="overview">{movieDetails.overview}</p>
+        </div>
 
-      <h1 className="genre-title">genres :</h1>
-      <ul className="genres">
-        {genreList}
-      </ul>
-      <p className="overview">{movieDetails.overview}</p>
+
+       <div className="box runtime-container">
+          <h1 className="title rating-title">Rating</h1>
+          <p className="average-rating">{movieDetails.average_rating}</p>
+        </div>
+
+        <div className="box runtime-container">
+          <h1 className="title runtime-title">Time</h1>
+          <p className="runtime">{movieDetails.runtime === 0 ? 'No runtime found' : `${movieDetails.runtime} mins.`}</p>
+        </div>
+
+        <div className="box budget-container">
+          <h1 className="title budget-title">budget</h1>
+          <p className="budget">{movieDetails.budget === 0 ? 'No budget found' : `$ ${movieDetails.budget}`}</p>
+        </div>
+
+        <div className="box revenue-container">
+          <h1 className="title revenue-title">revenue</h1>
+          <p className="revenue">{movieDetails.revenue === 0 ? 'No revenue found' : `$ ${movieDetails.revenue}`}</p>
+        </div>
+
+        <div className="box release-container">
+          <h1 className="title release-title">release date</h1>
+          <p className="release">{movieDetails.release_date === 0 ? 'No release date found' : moment(movieDetails.release_date).format("MMM Do YY")}</p>
+        </div>
+        <div className="box genre-container">
+        <h1 className="title genre-title">genres :</h1>
+        <ul className="genres">
+          {genreList}
+        </ul>
+        </div>
+
+        
+      </div>
     </div>
+ 
   )
 }
 
