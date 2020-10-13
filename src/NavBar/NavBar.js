022cd5ecf8c23005cae5f89840737f2a01eeefc9
Orlando Murcio <ato.murcio@gmail.com>
2020-10-13 14:27:53 -0600

    🔘 Add home button

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -10,17 +10,22 @@ export const NavBar = (props) => {
         <nav className="nav-bar">
           <h1 className="main-title"><i className="fas fa-seedling"></i>Rancid Tomatillos</h1>
           <div className="search-container">
-            <i className="fas fa-search"></i>
-            <label htmlFor="search-bar">
+            {/* <i className="fas fa-search"></i> */}
+            {/* <label htmlFor="search-bar">
             <input
               className="search-bar"
               type="text" 
               id="searh-bar"/>
-            </label>
+            </label> */}
           </div>
-          <Link to={name ? '/' :'/login'}>
-          <button onClick={logOut} className="Login-button">{name ? 'Log out' : 'Log in'}</button>
-          </Link>
+          <div className='button-container'>
+            <Link to= '/' >
+            <button className="Login-button">home</button>
+            </Link>
+            <Link to={name ? '/' :'/login'}>
+            <button onClick={logOut} className="Login-button">{name ? 'Log out' : 'Log in'}</button>
+            </Link>
+          </div>
         </nav>
     )
 }
