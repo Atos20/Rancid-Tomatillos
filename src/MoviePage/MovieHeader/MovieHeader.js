9e79a6374548847dc2ef247f8603e07620aca985
Ian Holladay <holladayian@gmail.com>
2020-10-13 10:11:21 -0800

    Install and implement 'Select'

--- src/MoviePage/MovieHeader/MovieHeader.js
+++ src/MoviePage/MovieHeader/MovieHeader.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import './MovieHeader.scss';
+import Select from 'react-select';
 
 export default class MovieHeader extends Component {
     constructor(props) {
         super(props);
-        this.ratingValue = document.querySelector('.rating-select')
+        this.state = {
+            selectedOption: null,
+          }
+          this.ratingValue = [
+              { value: 1, label: '1' },
+              { value: 2, label: '2' },
+              { value: 3, label: '3' },
+              { value: 4, label: '4' },
+              { value: 5, label: '5' },
+              { value: 6, label: '6' },
+              { value: 7, label: '7' },
+              { value: 8, label: '8' },
+              { value: 9, label: '9' },
+              { value: 10, label: '10' },
+              
+            ];
+        }
+
+    handleChange = (selectedOption) => {
+    this.setState({ selectedOption });
+    console.log(`Option selected:`, selectedOption);
     }
 
     addRating = () => {
-        this.props.addRating('placeholder value')
+        this.props.addRating(this.state.selectedOption)
     }
 
     render() {
@@ -19,18 +40,7 @@ export default class MovieHeader extends Component {
             {/* this will need to be in a conditional of only being able to bee seen if a user is logged in */}
             <div className="rate-movie">
                 <button className="delete-rating">Delete Rating</button>
-                <select className="rating-select" id="rating-select">
-                    <option className="value" value="1">option 1</option>
-                    <option value="2">option 2</option>
-                    <option value="3">option 3</option>
-                    <option value="4">option 4</option>
-                    <option value="5">option 5</option>
-                    <option value="6">option 6</option>
-                    <option value="7">option 7</option>
-                    <option value="8">option 8</option>
-                    <option value="9">option 9</option>
-                    <option value="10">option 10</option>
-                </select>
+                    <Select options={this.ratingValue} value={this.state.selectedOption} onChange={this.handleChange} />
                 <button className="add-rating" onClick={this.addRating}>Rate Movie</button>
             </div>
         </div>
