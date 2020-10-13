8352bbbfb1dd126757aa08d62a4eef7b42b2aba3
Ian Holladay <holladayian@gmail.com>
2020-10-13 07:16:01 -0800

    Add rating system buttoms

--- src/MoviePage/MovieHeader/MovieHeader.js
+++ src/MoviePage/MovieHeader/MovieHeader.js
@@ -6,6 +6,23 @@ export default function MovieHeader(props) {
         <div className="movie-header">
             <h1 className="title">{props.movieDetails.title}</h1>
             <h2 className="tagline">{props.movieDetails.tagline}</h2>
+            {/* this will need to be in a conditional of only being able to bee seen if a user is logged in */}
+            <div className="rate-movie">
+                <button className="delete-rating">Delete Rating</button>
+                <select id="mySelect">
+                    <option value="1">option 1</option>
+                    <option value="2">option 2</option>
+                    <option value="3">option 3</option>
+                    <option value="4">option 4</option>
+                    <option value="5">option 5</option>
+                    <option value="6">option 6</option>
+                    <option value="7">option 7</option>
+                    <option value="8">option 8</option>
+                    <option value="9">option 9</option>
+                    <option value="10">option 10</option>
+                </select>
+                <button className="add-rating">Rate Movie</button>
+            </div>
         </div>
     )
 }
