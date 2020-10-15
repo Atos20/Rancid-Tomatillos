065c08636b7779d4321a569292b10817c46207e8
Ian Holladay <holladayian@gmail.com>
2020-10-15 14:18:02 -0800

    Add movie header test

--- src/MoviePage/MovieCard/MovieCard.test.js
+++ src/MoviePage/MovieCard/MovieCard.test.js
@@ -1,25 +1,52 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import MovieCard from './MovieCard.js';
+import MovieCard from '../MovieCard/MovieCard.js';
 import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 describe('MovieCard', () => {
   it('should render the movie card', () => {
     const fakeMovieDetailProps = {
-      genres: ['horror', 'comedy'],
-      average_rating: 9, 
-      runtime: 115, 
-      budget: 2000000, 
-      revenue: 4000000, 
-      release_date: '1987/01/01' 
+      average_rating: 1,
+      backdrop_path: "https://fakebackdrop.jpg",
+      budget: 25,
+      genres: ["Action"],
+      id: 718444,
+      overview: "Battle-hardened O’Hara",
+      poster_path: "https://fakeimg.jpg",
+      release_date: "2020-08-20",
+      revenue: 24,
+      runtime: 106,
+      tagline: "When the hunter becomes the prey.",
+      title: "Rogue"
     };
-    // render(<MovieCard movieDetails={fakeMovieDetailProps}/>);
-    // expect(screen.getByText("9")).toBeInTheDocument();
-    // expect(screen.getByText("115 mins.")).toBeInTheDocument();
-    // expect(screen.getByText("$ 2000000")).toBeInTheDocument();
-    // expect(screen.getByText("$ 4000000")).toBeInTheDocument();
-    // expect(screen.getByText("Jan 1st 87")).toBeInTheDocument();
-    // expect(screen.getByText("horror")).toBeInTheDocument();
-    // expect(screen.getByText("comedy")).toBeInTheDocument();
+
+    const fakeRatedMovies = [
+      {id: 2650, user_id: 82, movie_id: 718444, rating: 3},
+      {id: 2661, user_id: 82, movie_id: 337401, rating: 8}
+    ]
+
+    render(
+      <Router>
+        <MovieCard movieDetails={fakeMovieDetailProps} ratedMovies={fakeRatedMovies}/>
+      </Router>
+    );
+      const fakeAverage = screen.getByText(/1.0/i);
+      const fakeUserRate = screen.getByText(/3.0/i);
+      const fakeScreenTime = screen.getByText("106 mins.");
+      const fakeBudget = screen.getByText("$ 25");
+      const fakeRevenue = screen.getByText("$ 24");
+      const fakeRelease = screen.getByText(/aug 20th 20/i);
+      const fakeGenre = screen.getByText("-Action");
+
+
+    expect(fakeAverage).toBeInTheDocument();
+    expect(fakeUserRate).toBeInTheDocument();
+    expect(fakeScreenTime).toBeInTheDocument();
+    expect(fakeBudget).toBeInTheDocument();
+    expect(fakeRevenue).toBeInTheDocument();
+    expect(fakeRelease).toBeInTheDocument();
+    expect(fakeGenre).toBeInTheDocument();
   })
 })
--- src/MoviePage/MovieHeader/MovieHeader.test.js
+++ src/MoviePage/MovieHeader/MovieHeader.test.js
@@ -2,12 +2,45 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import MovieHeader from '../MovieHeader/MovieHeader.js';
 import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 describe('MovieHeader', () => {
   it('should render the movie header', () => {
     const fakeMovieProps = {title: "Evil Dead", tagline: 'Groovey'}
-    render(<MovieHeader movieDetails={fakeMovieProps}/>);
+    render(
+      <Router>
+        <MovieHeader movieDetails={fakeMovieProps} />
+      </Router>
+    );
     expect(screen.getByText("Evil Dead")).toBeInTheDocument();
     expect(screen.getByText("Groovey")).toBeInTheDocument();
+    expect(screen.queryByText("Delete Rating")).not.toBeInTheDocument();
+
   })
+
+  it('should show the rating buttons when a user is loggen in', () => {
+    const fakeMovieProps = {title: "Evil Dead", tagline: 'Groovey'}
+    const fakeName = 'Bruce Cambell'
+    render(
+      <Router>
+        <MovieHeader movieDetails={fakeMovieProps} name={fakeName}/>
+      </Router>
+    );
+    expect(screen.getByText("Delete Rating")).toBeInTheDocument();
+    expect(screen.getByText("Rating")).toBeInTheDocument();
+    expect(screen.getByText("Rate Movie")).toBeInTheDocument();
+
+  })
+
+
+    // expect(screen.getByText("Groovey")).toBeInTheDocument();
+    // expect(screen.getByText("Groovey")).toBeInTheDocument();
+    // expect(screen.getByText("Groovey")).toBeInTheDocument();
+    // expect(screen.getByText("Groovey")).toBeInTheDocument();
+    // expect(screen.getByText("Groovey")).toBeInTheDocument();
+    // expect(screen.getByText("Groovey")).toBeInTheDocument();
+    // expect(screen.getByText("Groovey")).toBeInTheDocument();
+    // expect(screen.getByText("Groovey")).toBeInTheDocument();
+    // expect(screen.getByText("Groovey")).toBeInTheDocument();
 })
