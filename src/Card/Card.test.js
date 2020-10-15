25d0ec437bfb5a46ebfd5ec37afb34d844de55b2
Ian Holladay <holladayian@gmail.com>
2020-10-15 11:50:59 -0800

    Add Card test to test if a card is returned

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -7,15 +7,23 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('Card', () => {
   it('should return a card', () => {
-    // const movieDeets = {id: 1, title: "Jurassic Park", release_date: 1993, average_rating: 10, poster_path: 'http//fakeURLpath'}
-    // render(
-    //   <Router>
-    //     <Card movies={movieDeets}/>
-    //   </Router>
-    // )
-    // expect(screen.getByText("Jurassic Park")).toBeInTheDocument();
-    // expect(screen.getByText("1993")).toBeInTheDocument();
-    // expect(screen.getByText("10")).toBeInTheDocument();
-    // expect(screen.getByText("Rating")).toBeInTheDocument();
+    const movieDeets = {id: 1, title: "Jurassic Park", release_date: '1993-08-29', average_rating: 10, poster_path: 'http//fakeURLpath'}
+    // const fakeUserRating = {rating: 9};
+    const fakeUserRating = '';
+    render(
+      <Router>
+        <Card movies={movieDeets} ratedMovies={fakeUserRating}/>
+      </Router>
+    )
+    const movieName = screen.getByRole('heading', { name: /Jurassic Park/i });
+    const movieDate = screen.getByRole('heading', { name: /august 29, 1993/i });
+    const movieRating = screen.getByRole('heading', { name: /10/i });
+    const ratingTitle = screen.getByRole('heading', { name: /rating/i });
+    const notRated = screen.getByRole('heading', { name: /not rated/i });
+    expect(movieName).toBeInTheDocument();
+    expect(movieDate).toBeInTheDocument();
+    expect(movieRating).toBeInTheDocument();
+    expect(ratingTitle).toBeInTheDocument();
+    expect(notRated).toBeInTheDocument();
   })
 })
