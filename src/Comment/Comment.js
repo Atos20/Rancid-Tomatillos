6880e5fd59a269b2158de282174c50d33c4e0f17
Orlando Murcio <ato.murcio@gmail.com>
2020-10-15 20:33:27 -0600

    ⚙️ Turn Comment into a class Component

--- src/Comment/Comment.js
+++ src/Comment/Comment.js
@@ -1,27 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import './Comment.scss'
 
-export const Comment =() =>{
-  return (
-    <div className="comment-container">
-      <div className="inner-comment">
-        <input
-          value=""
-          type="text"
-          placeholder="add your comment here"
-          role="comment-section"
-          type="comment-section"
-          maxlength = "120"
-        />
-        <button 
-        role="commnt-button"
-        type="commnt-button"
-        onClick=""
-        className="comment-button">
-        add comment
-        </button>
-      </div>
-    </div>
-  )
+export class Comment extends Component  {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            commentValue : ''
+        }
+    }
+
+
+    render() {
+
+
+        return (
+          <div className="comment-container">
+            <div className="inner-comment">
+              <input
+                value=""
+                type="text"
+                placeholder="add your comment here"
+                role="comment-section"
+                type="comment-section"
+                maxlength = "120"
+                onChange=""
+              />
+              <button 
+              role="commnt-button"
+              type="commnt-button"
+              onClick=""
+              className="comment-button">
+              add comment
+              </button>
+            </div>
+          </div>
+        )
+    }
+}
+
+Comment.propTypes = {
+    commentValue : PropTypes.string,
+    updateValue : PropTypes.func,
+    getValue : PropTypes.func
 }
