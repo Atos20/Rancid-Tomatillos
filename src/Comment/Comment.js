5a9da4b2a92108bf80cb435af43b8cf6d172ab29
Orlando Murcio <ato.murcio@gmail.com>
2020-10-18 13:20:51 -0600

    🎨 Style comment buttons

--- src/Comment/Comment.js
+++ src/Comment/Comment.js
@@ -8,17 +8,21 @@ export const Comment = (props) => {
       role="comments"
       type="comments"
       className="comments">
+
       <div className="user-container">
         <h1 className="user">{props.comment.author}</h1> 
         <h1 className="time">3 months ago</h1> 
       </div>
+
      <p className="comment-text">{props.comment.comment}</p>
+     
      <div className="buttons-container">
        <i className="far fa-thumbs-up"></i>
-       <h3 className="like-count">1</h3>
+       <h3 className="like-count">0</h3>
        <i className="far fa-thumbs-down"></i>
        <i className="far fa-trash-alt"></i>
      </div>
+
     </div>
 
   )
